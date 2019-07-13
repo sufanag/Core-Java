61604caa1c9379c2f6beadb6494e8a868f33ff27
sufanag <52842335+sufanag@users.noreply.github.com>
2019-07-14 00:27:55 +0530

    adding core java program libraries

---
+++ src/com/qa/util/Main.java
@@ -0,0 +1,13 @@
+package com.qa.util;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+
+		Object data[][] = TestUtil.getTestData("contacts");
+		
+		System.out.println(data);
+	}
+
+}
